46370d37710aed902d262bb3f1943b8d73603167
DevKenyi <119520085+DevKenyi@users.noreply.github.com>
2024-03-29 15:24:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, Router, Routes } from "react-router-dom";
+import Signup, { SignUp } from "./auth/Signup";
+import OnboardingConnectStep from "./onboarding/OnboardingConnectStep";
+import OnboardingIntroStep from "./onboarding/OnboardingIntroStep";
+import OnboardingMain from "./onboarding/OnboardingMain";
+import OnboardingScheduleAppointment from "./onboarding/OnboardingScheduleAppointment";
+import OnboardingStayInformed from "./onboarding/OnboardingStayInformed";
+import OnboardingWelcomeStep from "./onboarding/OnboardingWelcomeStep";
+import { SignIn } from "./auth/Signin";
+
+function App() {
+  return (
+    <>
+      <OnboardingMain /> */
+      
+    </>
+  );
+}
+
+export default App;
